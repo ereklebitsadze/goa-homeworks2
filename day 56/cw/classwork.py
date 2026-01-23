0121e7011f5672536d8611dfe5f53912e2e1892e
ereklebitsadze <ereklebitsadze11@gmail.com>
2026-01-23 21:08:14 +0400

    second commit

---
+++ day 56/cw/classwork.py
@@ -0,0 +1,12 @@
+person = {
+    "name": "Erekle",
+    "age": 15,
+    "brand": "Jeep",
+    "engine": "2.8L Diesel",
+    "available_cars": ["Prius", "Mercedes", "BMW"]
+}
+
+print(person)
+
+for name in person:
+    print(name)
---
+++ day 56/cw/classwork2.py
@@ -0,0 +1,11 @@
+person = {
+    "name": "erekle",
+    "age": 15,
+    "brand": "Jeep",
+    "engine": "2.8L Diesel",
+    "available_cars": ["Prius", "Mercedes", "BMW"]
+}
+
+for key in person:
+    if key == "name":
+        print(person["name"])
